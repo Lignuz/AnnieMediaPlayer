6b55a4dd699f9860c1ae4d437681351941d31c82
illierouat <illierouat@gomcorp.com>
2025-05-07 16:17:23 +0900

    환경 설정에 언어 설정 (연동), 테마 설정(인터페이스)

--- AnnieMediaPlayer/Windows/Settings/Pages/GeneralSettingsPage.xaml.cs
+++ AnnieMediaPlayer/Windows/Settings/Pages/GeneralSettingsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using AnnieMediaPlayer.Options;
+using System.Windows.Controls;
 
 namespace AnnieMediaPlayer.Windows.Settings.Pages
 {
@@ -10,6 +11,21 @@ namespace AnnieMediaPlayer.Windows.Settings.Pages
         public GeneralSettingsPage()
         {
             InitializeComponent();
+
+            // Binding 으로 처리한 텍스트는 동적 변경이 안되므로 
+            // 언어 변경을 감지해서 다시 설정하도록 합니다. 
+            LanguageManager.LanguageChanged += (s, e) =>
+            {
+                var option = OptionViewModel.Instance.CurrentOption;
+
+                combo_languages.ItemsSource = null;
+                combo_languages.ItemsSource = option.AvailableLanguages;
+                combo_languages.SelectedItem = option.SelectedLanguage;
+
+                combo_themes.ItemsSource = null;
+                combo_themes.ItemsSource = option.AvailableThemes;
+                combo_themes.SelectedItem = option.SelectedTheme;
+            };
         }
     }
 }
